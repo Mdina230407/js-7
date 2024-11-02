5c435afc39feb29946dc4e117a8bbdbea9cb841a
Madina <madinash2304@gmail.com>
2024-11-02 17:23:14 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let all = +prompt ('введите число')
+let both = +prompt ('введите степень')
+
+let result =1;
+
+for(let i=0; i <both ; i++){
+    
+    result = result * all
+}
+console.log(result);
